37bae0f527c48c33f2853a4fb9464330d432210e
Astrol99 <dngames2006@gmail.com>
2025-03-03 22:58:38 -0600

    feat: added 3 new jokes

--- src/assets/insideJokes.tsx
+++ src/assets/insideJokes.tsx
@@ -115,6 +115,9 @@ const splashText: string[] = [
     `It's ${new Date().toLocaleString('en-US', { month: 'long', day: 'numeric' })} and OU still sucks`,
     'As seen on TV!	',
     "Should you major in Compsci? well, here's a better question. do you wanna have a bad time?",
+    'Is "bit" synonymous with "byte"? Really quick question before the test.',
+    'Some bytes of crumbl cookies would hit rn...',
+    'Ur not a big back if u take nibbles instead of bytes',
 ];
 
 export default splashText;
