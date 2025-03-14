8e3f39f7f1f49b6c1655d2cf9ea2617f633139a7
Kamsi-Elele <samuelelele209@gmail.com>
2025-03-13 19:12:35 -0500

    feat: rebrand plus to minus on UTRM

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -72,7 +72,7 @@ module.exports = {
                 ignoreReadBeforeAssign: false,
             },
         ],
-        'no-minusminus': 'off',
+        'no-plus': 'off',
         'no-inner-declarations': 'off',
         'sort-imports': 'off',
         'no-case-declarations': 'off',
