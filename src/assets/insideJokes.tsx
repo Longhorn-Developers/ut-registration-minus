95d1d3f5601ea5815880999af2e476b9332b5815
Pranav Belligundu <pranav@bubba.ai>
2025-03-28 18:24:38 -0500

    Update insideJokes.txs
    adding 3 new jokes

--- src/assets/insideJokes.tsx
+++ src/assets/insideJokes.tsx
@@ -115,6 +115,9 @@ const splashText: string[] = [
     `It's ${new Date().toLocaleString('en-US', { month: 'long', day: 'numeric' })} and OU still sucks`,
     'As seen on TV!	',
     "Should you major in Compsci? well, here's a better question. do you wanna have a bad time?",
+    "Data Science > Comp Sci",
+    "lol whats your registration time?",
+    "stealth startup uh, what do you do?"
 ];
 
 export default splashText;
